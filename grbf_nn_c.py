62b2e23a777087cef74504e953e2b101db66c61d
Danny D'Agostino <dannydagostino@hotmail.com>
2023-07-09 11:48:09 +0800

    Update grbf_nn_c.py

--- grbf_nn_c.py
+++ grbf_nn_c.py
@@ -177,9 +177,9 @@ class GRBF_NN_C:
         f = 1. / (1. + torch.exp(-S)) 
         return f
     
-    def feature_importance(self, X, P, features_names):
+    def feature_importance(self, X, P):
         
-        lambdas, U = eigh(P)
-        UL = np.dot(np.abs(U), lambdas)
-        FI = UL/sum(UL)
-        return FI, lambdas, U
+        gammas, V = eigh(P)
+        feature_importance = np.dot(np.abs(V), gammas)
+        feature_importance = feature_importance / sum(feature_importance)
+        return feature_importance, gammas, V
