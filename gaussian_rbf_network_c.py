2c2c4fc317179bc3ab659690b88217865760a54b
Danny D'Agostino <dannydagostino@hotmail.com>
2024-05-01 14:58:41 +0800

    add new files

--- gaussian_rbf_network_c.py
+++ gaussian_rbf_network_c.py
@@ -2,54 +2,48 @@ import torch
 import torch.nn as nn
 
 class GaussianRBFNetworkC(nn.Module):
-    def __init__(self, gaussian_regularizer, centers_regularizer, weights_regularizer, 
-                 centers, n_features, n_centers=None):
+    def __init__(self, gaussian_regularizer, centers_regularizer, weights_regularizer, learnable_centers,
+                 centers, device='cuda'):
         super(GaussianRBFNetworkC, self).__init__()
 
+        self.device = device
         self.gaussian_regularizer = gaussian_regularizer
         self.centers_regularizer = centers_regularizer
         self.weights_regularizer = weights_regularizer
         self.centers = centers
-        self.n_centers = n_centers
-        self.n_features = n_features
-
+        self.learnable_centers = learnable_centers
+        self.n_centers = centers.shape[0]
+        self.n_features = centers.shape[1]
         # Define learnable parameters
-
-        self.precision_elements = nn.Parameter(torch.randn(self.n_features * (self.n_features + 1) // 2))
-
-        if self.centers == None:
-            self.centers = nn.Parameter(torch.randn(self.n_centers, self.n_features))
+        self.precision_elements = nn.Parameter(torch.randn(self.n_features * (self.n_features + 1) // 2)
+                                               .to(self.device))
+        if self.learnable_centers == True:
+            self.centers = nn.Parameter(centers.to(self.device))
         else:
-            self.centers_regularizer = 1e-16
-        
-        
-        self.weights = nn.Parameter(torch.randn(self.n_centers))
+            self.centers_regularizer = 1e-32
+        self.weights = nn.Parameter(torch.randn(self.n_centers)
+                                    .to(self.device))
     
     def compute_precision(self, precision_elements):
         
-        lower_tri = torch.zeros((self.n_features, self.n_features))
+        lower_tri = torch.zeros((self.n_features, self.n_features)).to(self.device)
         idx = torch.tril_indices(*lower_tri.shape)
-        lower_tri[idx[0], idx[1]] = self.precision_elements
+        lower_tri[idx[0], idx[1]] = precision_elements
         precision_matrix = torch.matmul(lower_tri, lower_tri.T)
         return precision_matrix
     
     def forward(self, X):
         
-
         precision_matrix = self.compute_precision(self.precision_elements)
         diff = X[:, None, :] - self.centers.unsqueeze(0)
         Phi = torch.exp(-0.5 * torch.einsum('ijk, kl, ijl -> ij', diff, precision_matrix, diff))
-        
         y_hat = torch.sigmoid(Phi @ self.weights)
-        
         return y_hat
 
     def evaluate_loss(self, y, y_hat):
 
-        
         bce_loss = nn.BCELoss()
         loss = bce_loss(y, y_hat)
-
         reg_gaussian = self.gaussian_regularizer * torch.linalg.norm(self.precision_elements)
         reg_centers = self.centers_regularizer * torch.linalg.norm(self.centers)
         reg_weights = self.weights_regularizer * torch.linalg.norm(self.weights)
